6c82ad0959aadd5f7f93aace5d07a42db2233876
nitesh-dev <niteshdev547@gmail.com>
2023-02-01 22:51:49 +0530

    Initial commit

---
+++ app/src/main/java/com/flaxstudio/drawon/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.flaxstudio.drawon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.flaxstudio.drawon.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/flaxstudio/drawon/fragments/HomeFragment.kt
@@ -0,0 +1,31 @@
+package com.flaxstudio.drawon.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.flaxstudio.drawon.R
+import com.flaxstudio.drawon.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding.root.setOnClickListener{
+            findNavController().navigate(R.id.action_homeFragment_to_settingsFragment)
+        }
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
