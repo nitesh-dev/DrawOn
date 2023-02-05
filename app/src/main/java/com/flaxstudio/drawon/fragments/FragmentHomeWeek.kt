462498fa1a787ad2070e5b24944455703ebd50af
nitesh-dev <niteshdev547@gmail.com>
2023-02-05 21:47:57 +0530

    rectangle drawing features added

--- app/src/main/java/com/flaxstudio/drawon/fragments/FragmentHomeWeek.kt
+++
@@ -1,6 +0,0 @@
-package com.flaxstudio.drawon.fragments
-
-import androidx.fragment.app.Fragment
-import com.flaxstudio.drawon.R
-
-class FragmentHomeWeek : Fragment(R.layout.fragment_home_week)
--- app/src/main/java/com/flaxstudio/drawon/viewmodels/MainActivityViewModel.kt
+++ app/src/main/java/com/flaxstudio/drawon/viewmodels/MainActivityViewModel.kt
@@ -2,10 +2,12 @@ package com.flaxstudio.drawon.viewmodels
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.flaxstudio.drawon.utils.ProjectData
 
 class MainActivityViewModel: ViewModel() {
 
     val message =  MutableLiveData<String>()
+    val allProjects = MutableLiveData<ArrayList<ProjectData>>()
 
     fun sendMessage(msg: String){
         message.value = msg
