e003ae8dd7e3948a7304a49f290f525c6c68e0ea
nitesh-dev <niteshdev547@gmail.com>
2023-03-24 15:09:49 +0530

    fullscreen added in SplashScreenActivity.kt

--- app/src/main/java/com/flaxstudio/drawon/SplashScreenActivity.kt
+++ app/src/main/java/com/flaxstudio/drawon/SplashScreenActivity.kt
@@ -1,14 +1,40 @@
 package com.flaxstudio.drawon
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import android.view.WindowInsets
+import com.flaxstudio.drawon.databinding.ActivityMainBinding
+import com.flaxstudio.drawon.databinding.ActivitySplashScreenBinding
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // hide status bar and enable fullscreen
+        val rootView = binding.root
+
+        if (Build.VERSION.SDK_INT >= 30) {
+            rootView.windowInsetsController?.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+        } else {
+            // Note that some of these constants are new as of API 16 (Jelly Bean)
+            // and API 19 (KitKat). It is safe to use them, as they are inlined
+            // at compile-time and do nothing on earlier devices.
+            rootView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                        View.SYSTEM_UI_FLAG_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        }
 
 
         Handler().postDelayed({
