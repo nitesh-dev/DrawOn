5296ffc437f2ae853245f61a1cca35b9227c9ba6
Anubhav pathak <88669266+Anubhav0114@users.noreply.github.com>
2023-03-24 14:43:30 +0530

    Splash Screen Added

---
+++ app/src/main/java/com/flaxstudio/drawon/SplashScreenActivity.kt
@@ -0,0 +1,19 @@
+package com.flaxstudio.drawon
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+
+        Handler().postDelayed({
+            startActivity(Intent(this , MainActivity::class.java))
+            finish()
+        },3000)
+    }
+}
